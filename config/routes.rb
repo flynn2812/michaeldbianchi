8fad0e07a22544d887c011f64238897d96549aef
flynn2812 <michaeldbianchi@gmail.com>
2016-12-04 21:50:40 +0000

    Modify resume file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'contact_form/new'
+  #get 'contact_form/new'
 
-  get 'contact_form/create'
+  #get 'contact_form/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get 'application/hello'
+  #get 'application/hello'
   
-  resources :contact_forms
+  #resources :contact_forms
 end
